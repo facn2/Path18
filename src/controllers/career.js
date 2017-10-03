c5d1ddb1fad9b39d890df394d571cbedb09e48de
Vered Rekanati <veredrec@gmail.com>
2017-10-03 16:17:28 +0300

    change htmlHead file to html_head Relates #29

--- src/controllers/career.js
+++ src/controllers/career.js
@@ -1,14 +1,3 @@
-// const career = require('./../../database/career_model.js');
-// module.exports = (req, res) => {
-//   career.findById(req.params.id, (err, result) => {
-//     if (err) {
-//       console.log('Error with finding data ', err);
-//     } else {
-//       res.render('career', { career: result });
-//     }
-//   });
-// };
-
 module.exports = (req, res) => {
   res.render('career');
 };
