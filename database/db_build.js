57700083f883efc033b5a15f59dec8472c070e9e
heathercoraje <yhk7830@hotmail.com>
2017-10-03 13:19:38 +0300

    initial setup for db relates #16

--- database/db_build.js
+++ database/db_build.js
@@ -1,6 +1,7 @@
 const career = require('./career_model.js');
 const db = require('./db_connection.js');
 const careers = require('./seed_data.js');
+const mongoose = require('mongoose');
 
 db.once('open', () => {
   console.log('we are connected to DB');
@@ -20,7 +21,7 @@ db.once('open', () => {
     });
   }
 
-  function exit() {
+  function exit () {
     mongoose.disconnect();
   }
-})
+});
