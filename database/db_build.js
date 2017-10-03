4c9521481048ed915d8511a07064c3925b72faa2
heathercoraje <yhk7830@hotmail.com>
2017-10-03 15:58:26 +0300

    change for loop to forEach relates #16

--- database/db_build.js
+++ database/db_build.js
@@ -1,24 +1,21 @@
 const career = require('./career_model.js');
 const db = require('./db_connection.js');
 const careers = require('./seed_data.js');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 db.once('open', () => {
   console.log('we are connected to DB');
   career.collection.drop(); // once app is open, drop schema career
 
-careers.forEach((career) => {
-  career.save((error,result) => {
-    if (error) {
-      console.log(error);//render error hbs
-      return
+  careers.forEach((career) => {
+    career.save((error, result) => {
+      if (error) {
+        // render error hbs
+        return console.log(error);
       }
     });
-  })
+  });
 });
-
-
-
 
 // not sure if we need this
 // function exit () {
