1ec5cf3bdaa87e02fc13e06bbcd021eda28eb679
matthewdking <matthew.d.king@outlook.com>
2017-10-04 14:19:04 +0300

    changes to err for consistency relates #30

--- src/controllers/career.js
+++ src/controllers/career.js
@@ -1,8 +1,8 @@
 const Career = require('./../../database/career_model.js');
 
 module.exports = (req, res) => {
-  Career.find({}, (error, result) => {
-    if (error) {
+  Career.find({}, (err, result) => {
+    if (err) {
       return res.render('error', {
         statusCode: 503,
         message: 'Sorry cannot find anything in the database',
