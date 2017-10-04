20b5502311578ce0b11768f3ee9830bec5eefe07
heathercoraje <yhk7830@hotmail.com>
2017-10-04 12:56:00 +0300

    render mock data yet still in progress for adding codes for drop schema

--- database/db_build.js
+++ database/db_build.js
@@ -1,10 +1,7 @@
 const careers = require('./seed_data.js');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const db = require('./db_connection.js');
 const Career = require('./career_model.js');
-
-
-
 db.once('open', () => {
   Career.insertMany(careers);
   console.log('buidling db');
